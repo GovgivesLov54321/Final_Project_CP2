a621a6ea42e8b45dae9d19cd3a40e3023f822087
AllyB23 <marlin.rosales@ucas-edu.net>
2026-04-29 09:36:15 -0600

    pseudo

---
+++ docs/Pseudcode/Ally's Folder/pseudocode_Ally.py
@@ -0,0 +1,48 @@
+# AR 1st Pseudocode for Final
+
+# USER INTERFACE - MAIN MENU
+# import Tkinter as TK
+# Configure the looks of the GUI
+# DEF FUNCTION FOR INTRO
+    # Show text
+
+# DEF FUNCTION 
+
+
+
+
+
+# 
+# LOG IN - CREATE ACC
+# Import Hashlib and Import User resgistration
+# Import os
+
+# Def clear():
+    # Check os system
+
+# LOG IN FUNCTION
+
+# Def Log in():
+    # Information
+
+    # While True loop
+        # Clear 
+        # If statement 
+"""func clear
+const information
+func log_in
+func view_delete
+func sign_out
+func view_profile
+func valid_password"""
+
+# DEF View_delete():
+ # Pass
+
+# DEF Signout():
+ # pass
+
+# DEF View_Profile():
+    # pass
+
+# DEF Validated_Password():
