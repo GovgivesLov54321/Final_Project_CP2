9093ca602f7614388e837dd71256632d0925a6f1
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2026-05-06 09:52:00 -0600

    did sum coding

--- GovStuffosns/leaderboard.py
+++ GovStuffosns/leaderboard.py
@@ -1,5 +1,7 @@
 # GNB - Code to make leaderboard look pretty
 import csv
+import pygame
+
 # End up here from Ally's main menu if user wants to view the leaderboard:
 
 # Display options to User as: 1. View Leaderboard -- 2. Return to Main Menu
@@ -23,15 +25,22 @@ def order_scores():
 
 
 # Define function as prettify_list():
+def prettify_list():
+    print("===== HIGH SCORES =====")
 	# take from Ally’s renewed scores list, and print for every row in the csv in a certain format: (f"RANK NUMBER {row["rank number"]} == USERNAME {row["username"]} == SCORE {row["score"]})
+    print(f"RANK NUMBER {row["rank number"]} == USERNAME {row["username"]} == SCORE {row["player one score"]}")
+    
 
 # I believe this’ll also be used when showing the User if they made leaderboard or not: If this is the case, then just run the option 1
 
-# If 1: 
-    # Print the “=====HIGH SCORES=====” title thing up top
-    # Run the “order_scores” function
-    # Run the “prettify_list” function
+# Define function as leaderboard_main():
+def leaderboard_main():
+    # If 1: 
+        # Print the “=====HIGH SCORES=====” title thing up top
+        # Run the “order_scores” function
+        # Run the “prettify_list” function
 
 
-# If 2:
-    # Return to menu
+    # If 2:
+        # Return to menu
+
