e0345a14c3256cb8f1663daa15264fd1b7027cb8
AllyB23 <marlin.rosales@ucas-edu.net>
2026-05-07 09:29:44 -0600

    pygame

--- Ally's Testing/log_in.py
+++ Ally's Testing/log_in.py
@@ -3,6 +3,25 @@
 import hashlib
 from user_registration import *
 import os
+import pygame
+import csv
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("User Log In")
+
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for even in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    screen.fill((0, 0, 0))        
+
 
 # Helper function to clear the console
 def clear():
